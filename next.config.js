2be9a09baecff0725d6e8ad6b70948b7cb93be46
Jon Pepler <jonathan.pepler@and.digital>
2023-06-15 09:49:33 +0100

    ci: Update Next.js config for static export and GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+}
 
 module.exports = nextConfig
+
